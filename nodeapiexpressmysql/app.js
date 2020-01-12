794800ed6f7a12f08efb6cafbc6f1e1bf9db08b7
tharindu97 <tharindukavishna@gmail.com>
2020-01-12 19:36:03 +0530

    connect_mysql

--- nodeapiexpressmysql/app.js
+++ nodeapiexpressmysql/app.js
@@ -2,8 +2,35 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mysql = require('mysql')
 
-app.use(morgan('short'))
+app.use(morgan('combined'))
+
+app.get('/user/:id', (req, res)=>{
+    console.log('panama ' + req.params.id)
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'apinode'
+    })
+
+    const userId = req.params.id
+    const queryString = 'SELECT * FROM user WHERE id = ?'
+    connection.query(queryString,[userId], (err, rows, fields) => {
+        if(err){
+            console.log('Failed to query for users' + err)
+            res.sendStatus(500)
+            return
+        }
+        console.log('connect')
+
+        const users = rows.map((row) =>{
+            return {firstName: row.firstname, lastName: row.lastname}
+        }) 
+        res.json(users)
+    })
+})
 
 
 app.get('/', (req, res) =>{
