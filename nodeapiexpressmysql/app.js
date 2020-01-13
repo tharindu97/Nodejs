37ca16c4f6e2d94f45571f630858ce9745bb4f26
tharindu97 <tharindukavishna@gmail.com>
2020-01-13 10:27:19 +0530

    create_from

--- nodeapiexpressmysql/app.js
+++ nodeapiexpressmysql/app.js
@@ -3,17 +3,44 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 
-app.use(morgan('combined'))
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/user/:id', (req, res)=>{
-    console.log('panama ' + req.params.id)
+app.use(express.static('./public'))
 
-    const connection = mysql.createConnection({
+app.use(morgan('short'))
+
+app.post('/user_create', (req, res) =>{
+    console.log('Trying to create a new user....')
+
+    const firstName = req.body.create_first_name
+    const lastName = req.body.create_last_name
+
+    const queryString = 'INSERT INTO user (firstname, lastname) VALUES (?, ?)'
+    getConnection().query(queryString, [firstName, lastName], (err, results, fields)=>{
+        if(err){
+            console.log('Failed to insert new user: ' + err)
+            res.sendStatus(500)
+            return
+        }
+        console.log('Insert a new user with id: ', results.insertedId)
+        res.end()
+    })
+})
+
+function getConnection() {
+    return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         database: 'apinode'
     })
+}
+
+app.get('/user/:id', (req, res)=>{
+    console.log('panama ' + req.params.id)
+
+    const connection = getConnection()
 
     const userId = req.params.id
     const queryString = 'SELECT * FROM user WHERE id = ?'
@@ -29,6 +56,26 @@ app.get('/user/:id', (req, res)=>{
             return {firstName: row.firstname, lastName: row.lastname}
         }) 
         res.json(users)
+    })
+})
+
+app.get('/user', (req, res)=>{
+    console.log('panama ' + req.params.id)
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'apinode'
+    })
+    const queryString = 'SELECT * FROM user'
+    connection.query(queryString,(err, rows, fields) => {
+        if(err){
+            console.log('Failed to query for users' + err)
+            res.sendStatus(500)
+            return
+        }
+        console.log('connect')
+        res.json(rows)
     })
 })
 
